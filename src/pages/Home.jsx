636d60b267a8fbf22c283815baf7b1e567994220
helmdyck <helmuth.dyck@gmail.com>
2024-01-29 00:42:03 -0300

    perspective css improved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,46 +13,43 @@ function Home() {
   return (
     <>
       <div className="parallax__container">
-        
         <div className="header">
-          <img src={fourth} alt="parallax" className="fourth"/>
-          <img src={third} alt="parallax" className="third"/>
+          <img src={fourth} alt="parallax" className="fourth" />
+          <img src={third} alt="parallax" className="third" />
           <h1 className="title">DISCOVER BOLIVIA</h1>
-          <img src={second} alt="parallax" className="second"/>
-          <img src={first} alt="parallax" className="first"/>
+          <img src={second} alt="parallax" className="second" />
+          <img src={first} alt="parallax" className="first" />
         </div>
 
         <section className="categories">
-        <div className="image__gallery">
-          <div className="image__card" id="image__car-1">
-            <img src={cities} alt="grid" />
+          <div className="image__gallery">
+            <div className="image__card" id="image__car-1">
+              <img src={cities} alt="grid" />
+            </div>
+            <div className="image__card" id="image__car-2">
+              <img src={forests} alt="grid" />
+            </div>
+            <div className="image__card" id="image__car-3">
+              <img src={valleys} alt="grid" />
+            </div>
+            <div className="image__card" id="image__car-4">
+              <img src={lakes} alt="grid" />
+            </div>
           </div>
-          <div className="image__card" id="image__car-2">
-            <img src={forests} alt="grid" />
+          <div className="details">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              asperiores consectetur eius optio aspernatur nobis et obcaecati?
+              Corrupti corporis, quo rem possimus sequi doloribus enim neque
+              minima, eius recusandae a. Ratione voluptates alias quos, deleniti
+              mollitia ad totam voluptatem nostrum consequuntur.
+            </p>
           </div>
-          <div className="image__card" id="image__car-3">
-            <img src={valleys} alt="grid" />
+          <div className="footer__btn">
+            <button className="learn__more">Learn More</button>
           </div>
-          <div className="image__card" id="image__car-4">
-            <img src={lakes} alt="grid" />
-          </div>
-        </div>
-        <div className="details">
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam
-            asperiores consectetur eius optio aspernatur nobis et obcaecati?
-            Corrupti corporis, quo rem possimus sequi doloribus enim neque
-            minima, eius recusandae a. Ratione voluptates alias quos, deleniti
-            mollitia ad totam voluptatem nostrum consequuntur.
-          </p>
-        </div>
-        <div className="footer__btn">
-          <button className="learn__more">Learn More</button>
-        </div>
-      </section>
+        </section>
       </div>
-
-      
     </>
   );
 }
